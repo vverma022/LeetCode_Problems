0242805248f8566b81ffbce62392b7d98b4eb914
Vasu Verma <vermavasu069@gmail.com>
2024-05-30 14:42:37 +0530

    Added questions 1404

---
+++ src/BitManipulation/NumberofStepstoReduceaNumber.java
@@ -0,0 +1,27 @@
+package BitManipulation;
+
+public class NumberofStepstoReduceaNumber {
+    public static void main(String[] args) {
+        String s = "1101";
+        System.out.println(numSteps(s));
+    }
+
+    public static int numSteps(String s) {
+        char[] sarr = s.toCharArray();
+        int n = sarr.length;
+        int carry = 0;
+        int count = 0;
+        for(int i = n - 1; i > 0; i--){
+            if(sarr[i] - '0' + carry == 1){
+                carry = 1;
+                count += 2;
+        } else {
+            if(carry == 1){
+                count++;
+            }
+            count++;
+        }
+    }
+    return count + carry;
+    }
+}
