3eb52e51b03385e983b39ea173d32c48cd7266cb
Vasu Verma <vermavasu069@gmail.com>
2024-07-02 16:47:59 +0530

    Added question 350

---
+++ src/ArraysHashMap/IntersectionofTwoArrays2.java
@@ -0,0 +1,35 @@
+package ArraysHashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IntersectionofTwoArrays2 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+    }
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+
+        }
+        int[] result = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            result[k] = list.get(k);
+        }
+        return result;
+    }
+}
